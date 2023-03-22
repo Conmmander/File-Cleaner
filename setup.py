46e3a0cee400b19fadbb91adf1ba1a90a0f8da68
Ryan <ryandodd0115@gmail.com>
2023-03-22 02:35:04 -0500

    Deployment finished, distribution included. Testing to commence later

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 import py2exe
-
+# python -m setup
 py2exe.freeze(
-    console=["main.py"],
+    windows=["main.py"],
+    options={'bundle_files': 0, 'compressed': True},
+    version_info={'version': '0.1.0', 'product_name': 'File Cleaner', 'copyright': 'Ryan Dodd © 2023', 'description': 'File Cleaner'},
 )
