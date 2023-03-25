6410ae5ecd7562425396b9da464229e4644212ef
Ryan <ryandodd0115@gmail.com>
2023-03-25 17:10:11 -0500

    Working file cleaner and fixed setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ import py2exe
 # python -m setup
 py2exe.freeze(
     windows=["main.py"],
-    options={'bundle_files': 0, 'compressed': True},
+    options={'bundle_files': 1},
     version_info={'version': '0.1.0', 'product_name': 'File Cleaner', 'copyright': 'Ryan Dodd © 2023', 'description': 'File Cleaner'},
 )
